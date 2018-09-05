8fe98d1bb9e5386c0355b1e1220dcd2397fe5ae8
Alba Heidari <albaheidari@gmail.com>
2018-09-05 14:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  var names = ["Milo", "Otis", "Garfield"];
-  names.splice(3, 0, "Ralph");
-  return names;
+  var kittens = ["Milo", "Otis", "Garfield"];
+  kittens.splice(3, 0, "Ralph");
+  return kittens;
 }
