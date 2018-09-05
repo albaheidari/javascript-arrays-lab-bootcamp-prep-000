63f568b9228108ef9b6fb765aec6fbddf27f1eb2
Alba Heidari <albaheidari@gmail.com>
2018-09-05 14:53:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// Add your functions and code here
+ var kittens = ["Milo", "Otis", "Garfield"];/
 function destructivelyAppendKitten(name){
-  var kittens = ["Milo", "Otis", "Garfield"];
-  kittens.push("Ralph");
+ kittens.push("Ralph")
   return kittens;
 }
